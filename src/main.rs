14ce8b2bb9cc5b7a83a2766290aeacedc424d3b0
nivanchenko <n1k17ka@gmail.com>
2025-01-03 01:36:16 +0300

    dif format

--- src/main.rs
+++ src/main.rs
@@ -39,8 +39,14 @@ fn main() {
             // event10  GESTURE_SWIPE_UPDATE +3.769s	4  0.25/ 0.48 ( 0.95/ 1.85 unaccelerated)
             let parts: Vec<&str> = pattern.split(&line).filter(|c| !c.is_empty()).collect();
             let action = parts[1];
-            let finger = parts[3];
-            if finger != "3" && !action.starts_with("GESTURE_HOLD"){
+            let finger;
+            if parts[3].contains("s") {
+                finger = parts[4];
+            } else {
+                finger = parts[3];
+            }
+            if !action.starts_with("GESTURE_HOLD") && finger != "3" {
+                //println!("not 3 and not hold{} finger {} action {} len {}",line, finger, action, parts.len());
                 xdo_handler.mouse_up(1);
                 continue;
             }
@@ -48,13 +54,16 @@ fn main() {
 
             match action {
                 "GESTURE_SWIPE_BEGIN" => {
+                    //println!("begin {}",line);
                     xsum = 0.0;
                     ysum = 0.0;
                     xdo_handler.mouse_down(1);
                 }
                 "GESTURE_SWIPE_UPDATE" => {
-                    let x: f32 = parts[4].parse().unwrap();
-                    let y: f32 = parts[5].parse().unwrap();
+                    //println!("update {} x {} y {}",line, parts[5], parts[6]);
+                
+                    let x: f32 = parts[5].parse().unwrap();
+                    let y: f32 = parts[6].parse().unwrap();
                     xsum += x * acceleration;
                     ysum += y * acceleration;
                     if xsum.abs() > 1.0 || ysum.abs() > 1.0 {
@@ -64,28 +73,33 @@ fn main() {
                     }
                 }
                 "GESTURE_SWIPE_END" => {
+                    //println!("end {}",line);
                     xdo_handler.move_mouse_relative(xsum as i32, ysum as i32);
                     if cancelled {
                         xdo_handler.mouse_up(1);
                     } else {
-                        xdo_handler.mouse_up_delay(1, 600);
+                        xdo_handler.mouse_up_delay(1, 1000);
                     }
                 }
                 "GESTURE_HOLD_BEGIN" => {
+                    //println!("hold begin {}",line);
                     // Ignore
                 }
                 "GESTURE_HOLD_END" => {
+                    //println!("hold end {} c {}",line, cancelled);
                     // Ignore accidental holds when repositioning
-                    if !cancelled {
-                        xdo_handler.mouse_up(1);
-                    }
+                    // if !cancelled {
+                    //     xdo_handler.mouse_up(1);
+                    // }
                 }
                 _ => {
                     // GESTURE_PINCH_*,
+                    //println!("pinch {}",line);
                     xdo_handler.mouse_up(1);
                 }
             }
         } else {
+            //println!("else {}",line);
             xdo_handler.mouse_up(1);
         }
     }
